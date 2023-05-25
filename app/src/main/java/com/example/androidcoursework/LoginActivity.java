7d3c4d87336eb3cb332573bb4ceff7a6c080fd7d
nikoleta-dobrevska <nikoletadobrevska@gmail.com>
2023-05-25 16:52:11 +0300

    Implement firebase authorization

--- app/src/main/java/com/example/androidcoursework/LoginActivity.java
+++ app/src/main/java/com/example/androidcoursework/LoginActivity.java
@@ -1,25 +1,47 @@
 package com.example.androidcoursework;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.androidcoursework.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(LoginActivity.this, ChooseSubjectActivity.class);
+            startActivity(intent);
+
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
 
         Button loginButton = binding.loginButton;
         Button signUpButton = binding.signUpButton;
@@ -29,12 +51,34 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = emailTextView.getText().toString();
-                String password = passwordTextView.getText().toString();
+                String email = String.valueOf(emailTextView.getText());
+                String password = String.valueOf(passwordTextView.getText());
 
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Please enter valid username and password!", Toast.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Please enter valid email address!", Toast.LENGTH_LONG).show();
+                    return;
                 }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(), "Please enter valid password!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Intent intent = new Intent(LoginActivity.this, ChooseSubjectActivity.class);
+                                    startActivity(intent);
+
+                                    finish();
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
 
--- app/src/main/java/com/example/androidcoursework/RegisterActivity.java
+++ app/src/main/java/com/example/androidcoursework/RegisterActivity.java
@@ -2,15 +2,22 @@ package com.example.androidcoursework;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidcoursework.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.w3c.dom.Text;
 
@@ -19,6 +26,19 @@ import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(RegisterActivity.this, ChooseSubjectActivity.class);
+            startActivity(intent);
+
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +46,17 @@ public class RegisterActivity extends AppCompatActivity {
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mAuth = FirebaseAuth.getInstance();
+
         TextView emailTextView = binding.emailRegisterTextView;
         TextView passwordTextView = binding.passwordRegisterTextView;
         TextView confirmPasswordTextView = binding.confirmPasswordTextView;
         Button registerButton = binding.registerButton;
         TextView alreadyLoggedIn = binding.alreadyLoggedTextView;
 
-        String email = emailTextView.getText().toString();
-        String password = passwordTextView.getText().toString();
-        String confirmPassword = confirmPasswordTextView.getText().toString();
+        String email = String.valueOf(emailTextView.getText());
+        String password = String.valueOf(passwordTextView.getText());
+        String confirmPassword = String.valueOf(confirmPasswordTextView.getText());
 
         alreadyLoggedIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,13 +67,41 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Please enter valid email address!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(getApplicationContext(), "Please enter valid password!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(confirmPassword)){
+                    Toast.makeText(getApplicationContext(), "Please confirm password!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
                 if(password.compareTo(confirmPassword) == 0) {
                     if(validatePassword(password)) {
-                        startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
-                    } else{
+                        mAuth.createUserWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(RegisterActivity.this, "You have registered successfully!",
+                                                    Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+                    } else {
                         Toast.makeText(getApplicationContext(), "Password must contain at least 8 characters, a capital letter, a lowercase letter and a special character!", Toast.LENGTH_LONG).show();
                     }
                 } else {
